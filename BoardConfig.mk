cd98c6b84b34ab3203b0fee80aae58f4fb259050
Blerfont <blerfont@gmail.com>
2013-07-22 17:35:45 +0200

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,82 @@
+#
+# Copyright (C) 2012 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+TARGET_BOOTLOADER_BOARD_NAME := a10
+TARGET_BOARD_PLATFORM := sun4i
+
+USE_CAMERA_STUB := false
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB
+BOARD_USES_GENERIC_AUDIO := false
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+#CPU stuff
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAS_SDCARD_INTERNAL := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 268435456
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1073741824
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+#EGL stuff
+BOARD_EGL_CFG := device/softwinner/common/egl.cfg
+USE_OPENGL_RENDERER := true
+ENABLE_WEBGL := true
+BOARD_USE_SKIA_LCDTEXT := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+#Recovery Stuff
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_generic
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_generic
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/softwinner/common/recovery_keys.c
+TARGET_RECOVERY_PRE_COMMAND := "setrecovery"
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+#Headers stuff
+TARGET_SPECIFIC_HEADER_PATH := device/softwinner/common/include
+
+# Wifi stuff
+BOARD_WIFI_VENDOR                := realtek
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
+BOARD_HOSTAPD_DRIVER             := WEXT
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_rtl
+BOARD_WLAN_DEVICE                := rtl8192cu
+
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/8192cu.ko"
+WIFI_DRIVER_MODULE_NAME          := 8192cu
+
+TARGET_CUSTOM_WIFI := ../../hardware/realtek/wlan/wifi_realtek.c
+
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 rw init=/init loglevel=8
+BOARD_KERNEL_PAGESIZE := 2048
+
+TARGET_KERNEL_SOURCE := kernel/softwinner/common
+TARGET_KERNEL_CONFIG := sun4i_crane_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
---
+++ device.mk
@@ -0,0 +1,111 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.google.locationfeatures=1 \
+	ro.media.dec.jpeg.memcap=20000000 \
+	dalvik.vm.lockprof.threshold=500 \
+	dalvik.vm.dexopt-data-only=1 \
+	ro.vold.umsdirtyratio=20 \
+	hwui.render_dirty_regions=false \
+	persist.sys.usb.config=mass_storage,adb \
+	ro.display.switch=1 \
+	ro.opengles.version=131072 \
+	wifi.interface=wlan0 \
+	wifi.supplicant_scan_interval=60 \
+	ro.carrier=wifi-only \
+
+
+DEVICE_PACKAGE_OVERLAYS += device/softwinner/common/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/android.hardware.sensor.compass.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+
+# EXT4 Support
+PRODUCT_PACKAGES += make_ext4fs e2fsck
+
+# Hardware support
+PRODUCT_PACKAGES += audio.primary.sun4i \
+	audio_policy.default \
+	audio.a2dp.default \
+	audio.usb.default \
+	camera.sun4i \
+	display.sun4i \
+	gralloc.sun4i \
+	hwcomposer.sun4i \
+	lights.sun4i \
+	power.sun4i \
+	sensors.sun4i \
+	setrecovery \
+#
+
+# CedarX libraries
+PRODUCT_PACKAGES += libCedarA \
+	libCedarX \
+	libcedarv \
+	libcedarxbase \
+	libcedarxosal \
+	libcedarxsftdemux \
+	libswdrm \
+	libcedarv_adapter \
+	libve \
+	libfacedetection \
+	libaw_audio \
+	libaw_audioa \
+	libcedarv_base \
+	libstagefright_soft_cedar_h264dec \
+	librtmp \
+#
+
+# runs after recovery boot
+PRODUCT_COPY_FILES += device/softwinner/common/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+# and let's get booting!
+PRODUCT_COPY_FILES += device/softwinner/common/ueventd.sun4i.rc:root/ueventd.sun4i.rc \
+	device/softwinner/common/init.sun4i.rc:root/init.sun4i.rc \
+	device/softwinner/common/init.sun4i.usb.rc:root/init.sun4i.usb.rc \
+#
+
+# let us use the storage
+PRODUCT_COPY_FILES += device/softwinner/common/vold.fstab:system/etc/vold.fstab
+
+# include a wpa_supplicant.conf file
+PRODUCT_COPY_FILES += device/softwinner/common/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# OMX codec support
+PRODUCT_COPY_FILES += device/softwinner/common/media_codecs.xml:system/etc/media_codecs.xml
+
+# Audio policy
+PRODUCT_COPY_FILES += device/softwinner/common/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, build/target/product/full_base.mk)
+
+PRODUCT_NAME := full_a10
+PRODUCT_DEVICE := A10
